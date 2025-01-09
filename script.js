2483dd5721dd886b69d7614d5eaae43ec7649443
Shinas <113939134+shinaslanap@users.noreply.github.com>
2025-01-09 03:14:07 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function insert_Row() {
-    //Write your code here
+
+	function insert_Row() {
+    
+    var table = document.getElementById("sampleTable");
+    
+    
+    var newRow = table.insertRow(1); 
+
+    
+    var cell1 = newRow.insertCell(0); 
+    var cell2 = newRow.insertCell(1); 
+
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
+}
+
   
   
 }
